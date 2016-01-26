132208fa2477701e719dc6ee82c182259fc3f25c
Woohyun Kim <r45r54r45@naver.com>
2016-01-27 00:43:59 +0900

    dd

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -3,30 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Test extends CI_Controller {
 
-	public function t1()
+	public function index()
 	{
 		$this->load->view('header');
 		$this->load->view('js_test');
 		$this->load->view('footer');
 	}
-	public function t2()
-	{
-		$this->load->view('header');
-		$this->load->view('js_test2');
-		$this->load->view('footer');
-	}
-	public function t3()
-	{
-		$this->load->view('header');
-		$this->load->view('js_test3');
-		$this->load->view('footer');
-	}
-	public function t4()
-	{
-		$this->load->view('header');
-		$this->load->view('js_test4');
-		$this->load->view('footer');
-	}
+
 	public function share()
 	{
 		$this->load->view('facebook_share');
