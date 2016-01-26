301252ec0bd0095842c26f0dd24c08379dc624b1
Woohyun Kim <r45r54r45@naver.com>
2016-01-26 14:09:43 +0900

    dd

--- application/views/facebook_share.php
+++ application/views/facebook_share.php
@@ -1,10 +1,10 @@
 <html>
 <head>
   <meta property="fb:app_id" content="214340015575657"/>
-  <meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
+  <meta property="og:url"                content="http://blankit.kr" />
   <meta property="og:type"               content="article" />
-  <meta property="og:title"              content="When Great Minds Don’t Think Alike" />
-  <meta property="og:description"        content="How much does culture influence creative thinking?" />
+  <meta property="og:title"              content="jinwoo" />
+  <meta property="og:description"        content="cheon" />
   <meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
 </head>
 <body>
