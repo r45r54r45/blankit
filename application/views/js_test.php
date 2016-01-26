d56eb319ec15eb02e598481562cb2fdabc079318
Woohyun Kim <r45r54r45@naver.com>
2016-01-26 13:26:37 +0900

    dd

--- application/views/js_test.php
+++ application/views/js_test.php
@@ -1,43 +1,10 @@
-<style>
-.test{
-  position: relative;
-  width:300px;
-  height:300px;
-}
-.product_bed{
-  position: absolute;
-  left:0;
-  top:0;
-  z-index: 1;
-}
-.product{
-  position: absolute;
-  left:35.4%;
-  top:45.85%;
-  width:29.4%;
-  height:40%;
-  z-index:2;
-}
-</style>
 <script>
-
+function cl(){
+  FB.ui({
+  method: 'share',
+  href: 'https://www.facebook.com/dialog/share?app_id=145634995501895&amp;display=popup&amp;href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2F&amp;redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer',
+}, function(response){});
+}
 </script>
-</div>
-<div class="container">
-<div class="test" style="position: relative;">
-  <image class="product_bed img-responsive" src="/source/image/product_test_bed.png" style="  position: absolute;
-    left:0;
-    top:0;
-    z-index: 1;">
-  <div class="product" style="  position: absolute;
-    left:35.4%;top:46.4%;width:29.3%;height:39.93%;
-    z-index:2;">
-    <img src="/source/image/ecobag_test.png" class="img img-responsive">
-  </div>
-</div>
-</div>
-
-
-<!-- function cal(w,h,l,t,iw,ih){
-  return "left:"+l/w*100+"%;top:"+t/h*100+"%;width:"+iw/w*100+"%;height:"+ih/h*100+"%;";
-} -->
+<div style="height:400px;"></div>
+<button onclick="cl()">dd</button>
--- source/js/common.js
+++ source/js/common.js
@@ -8,37 +8,46 @@ $(function(){
     location.href="/aboutus";
   });
   var sideslider = $('[data-toggle=collapse-side]');
-           var sel = sideslider.attr('data-target');
-           var sel2 = sideslider.attr('data-target-2');
-           sideslider.click(function(event){
-               $(sel).toggleClass('in');
-               $(sel2).toggleClass('out');
-           });
+  var sel = sideslider.attr('data-target');
+  var sel2 = sideslider.attr('data-target-2');
+  sideslider.click(function(event){
+    $(sel).toggleClass('in');
+    $(sel2).toggleClass('out');
+  });
+  $.ajaxSetup({ cache: true });
+  $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
+    FB.init({
+      appId: '214340015575657',
+      version: 'v2.5' // or v2.0, v2.1, v2.2, v2.3
+    });
+    $('#loginbutton,#feedbutton').removeAttr('disabled');
+    FB.getLoginStatus(updateStatusCallback);
+  });
 });
 
 function setCookie(cookieName, value, exdays){
-    var exdate = new Date();
-    exdate.setDate(exdate.getDate() + exdays);
-    var cookieValue = escape(value) + ((exdays==null) ? "" : "; expires=" + exdate.toGMTString());
-    document.cookie = cookieName + "=" + cookieValue;
+  var exdate = new Date();
+  exdate.setDate(exdate.getDate() + exdays);
+  var cookieValue = escape(value) + ((exdays==null) ? "" : "; expires=" + exdate.toGMTString());
+  document.cookie = cookieName + "=" + cookieValue;
 }
 
 function deleteCookie(cookieName){
-    var expireDate = new Date();
-    expireDate.setDate(expireDate.getDate() - 1);
-    document.cookie = cookieName + "= " + "; expires=" + expireDate.toGMTString();
+  var expireDate = new Date();
+  expireDate.setDate(expireDate.getDate() - 1);
+  document.cookie = cookieName + "= " + "; expires=" + expireDate.toGMTString();
 }
 
 function getCookie(cookieName) {
-    cookieName = cookieName + '=';
-    var cookieData = document.cookie;
-    var start = cookieData.indexOf(cookieName);
-    var cookieValue = '';
-    if(start != -1){
-        start += cookieName.length;
-        var end = cookieData.indexOf(';', start);
-        if(end == -1)end = cookieData.length;
-        cookieValue = cookieData.substring(start, end);
-    }
-    return unescape(cookieValue);
+  cookieName = cookieName + '=';
+  var cookieData = document.cookie;
+  var start = cookieData.indexOf(cookieName);
+  var cookieValue = '';
+  if(start != -1){
+    start += cookieName.length;
+    var end = cookieData.indexOf(';', start);
+    if(end == -1)end = cookieData.length;
+    cookieValue = cookieData.substring(start, end);
+  }
+  return unescape(cookieValue);
 }
