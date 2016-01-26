9092d06124344086eab48d22125f742e79668528
Woohyun Kim <r45r54r45@naver.com>
2016-01-26 16:54:09 +0900

    nn

--- application/views/header.php
+++ application/views/header.php
@@ -11,9 +11,9 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
     <!-- 스토어 세부페이지와 submit 페이지에서만 출력 시작-->
     <?if(isset($_GET['url'])){?>
-    <meta property="fb:app_id" content="214340015575657"/>
-    <meta property="og:url"                content="<?echo $_GET['url'];?>" />
-    <meta property="og:type"               content="article" />
+    <meta property="fb:app_id" content="214340015575657"\/>
+    <meta property="og:url"                content="<?echo $_GET['url'];?>" \/>
+    <meta property="og:type"               content="website" \/>
     <meta property="og:title"              content="<?echo $_GET['title'];?>" />
     <meta property="og:description"        content="<?echo $_GET['description'];?>" />
     <meta property="og:image"              content="<?echo $_GET['image'];?>" />
