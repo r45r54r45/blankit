5a7173d5afcd8be07ca8184a07a3e001f31b9061
Woohyun Kim <r45r54r45@naver.com>
2016-01-26 14:15:54 +0900

    jj

--- application/views/facebook_share.php
+++ application/views/facebook_share.php
@@ -1,7 +1,7 @@
 <html>
 <head>
   <meta property="fb:app_id" content="214340015575657"/>
-  <meta property="og:url"                content="http://blankit.kr" />
+  <meta property="og:url"                content="http://blankit.kr/test/share" />
   <meta property="og:type"               content="article" />
   <meta property="og:title"              content="jinwoo" />
   <meta property="og:description"        content="cheon" />
