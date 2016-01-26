e29b7ae5bbe07c6f7241a0b5e3758cf995fe3d05
Woohyun Kim <r45r54r45@naver.com>
2016-01-26 16:49:36 +0900

    dd

--- application/views/js_test.php
+++ application/views/js_test.php
@@ -1,12 +1,12 @@
 <script>
-var url=encodeURIComponent("http://blankit.kr/store?url=http%3A%2F%2Fblankit.kr%2Fstore&title=테스트타이틀&description=테스트설명&image=http%3A%2F%2Fblankit.kr%2Fsource%2Fimage%2Fecobag_test.png");
+var url=encodeURIComponent("http://blankit.kr/store/product/5");
 var title='테스트타이틀';
 var description='테스트설명';
 var image=encodeURIComponent("http://blankit.kr/source/image/ecobag_test.png");
 function cl(){
   FB.ui({
   method: 'share',
-  href: 'http://blankit.kr/store?url='+url+'&title='+title+'&description='+description+'&image='+image,
+  href: 'http://blankit.kr/store/product/5?url='+url+'&title='+title+'&description='+description+'&image='+image,
 }, function(response){});
 }
 </script>
