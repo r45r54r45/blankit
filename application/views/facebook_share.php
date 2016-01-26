033ebefab5331c71ca584d715cc8a04372706093
Woohyun Kim <r45r54r45@naver.com>
2016-01-26 15:01:05 +0900

    dd

--- application/views/facebook_share.php
+++ application/views/facebook_share.php
@@ -8,11 +8,11 @@
 
   <!-- 스토어 세부페이지와 submit 페이지에서만 출력 시작-->
   <meta property="fb:app_id" content="214340015575657"/>
-  <meta property="og:url"                content="http://blankit.kr/test/share" />
+  <meta property="og:url"                content="<?echo $_GET['url'];?>" />
   <meta property="og:type"               content="article" />
-  <meta property="og:title"              content="jinwoo" />
-  <meta property="og:description"        content="cheon" />
-  <meta property="og:image"              content="http://blankit.kr/source/image/ecobag_test.png" />
+  <meta property="og:title"              content="<?echo $_GET['title'];?>" />
+  <meta property="og:description"        content="<?echo $_GET['description'];?>" />
+  <meta property="og:image"              content="<?echo $_GET['image'];?>" />
   <!-- 스토어 세부페이지와 submit 페이지에서만 출력 끝-->
 </head>
 <body>
