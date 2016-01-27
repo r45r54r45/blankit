aa7650dbfc5727e6afa0d9332c0c77f11f01e7f7
ArsisCheon <arsischeon@gmail.com>
2016-01-27 14:27:59 +0900

    x

--- application/controllers/File.php
+++ application/controllers/File.php
@@ -5,12 +5,12 @@ class File extends CI_Controller {
 
 	public function profile()
 	{
-		$uriPath = $this->uri->segment(3);
-		$idName = end(explode("=", $uriPath));
+		//$uriPath = $this->uri->segment(3);
+		//$idName = end(explode("=", $uriPath));
 
 		if (!empty($_FILES)) {
 			$ext = end(explode(".", $_FILES['userfile']['name']));
-			$fileName = $idName . "_profile." . $ext;
+			//$fileName = $idName . "_profile." . $ext;
 			$fileName = $_GET['file'] . "_profile." . $ext;
 			move_uploaded_file($_FILES['userfile']['tmp_name'],SITE_ROOT."/files/profile/". $fileName);
 		}
