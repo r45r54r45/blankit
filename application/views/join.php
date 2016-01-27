d19c5952d3ff5f3f4a0caaa0e2b7834e0aa487ac
Woohyun Kim <r45r54r45@naver.com>
2016-01-27 14:10:35 +0900

    mk

--- application/views/join.php
+++ application/views/join.php
@@ -82,7 +82,8 @@ function id_check(){
         $("#uuu").css("display","none");
         if ($('#profile').length) {
           var nameValue = $("input[name='id']").val();
-          myDropzone1 = new Dropzone("#profile", { url: "/file/profile?file=" + nameValue, paramName: "userfile",maxFiles:1,addRemoveLinks:true,autoProcessQueue: false,dictRemoveFile:"파일 삭제",dictMaxFilesExceeded:"1개만 업로드할 수 있습니다."});
+          var urll="/file/profile?file=" + nameValue;
+          myDropzone1 = new Dropzone("#profile", { url:urll , paramName: "userfile",maxFiles:1,addRemoveLinks:true,autoProcessQueue: false,dictRemoveFile:"파일 삭제",dictMaxFilesExceeded:"1개만 업로드할 수 있습니다."});
         };
         console.log(nameValue);
         alert('사용할 수 있는 아이디입니다.');
