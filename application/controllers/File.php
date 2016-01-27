5e6348a4b5eb616121c2d5c814a8a29592d30340
ArsisCheon <arsischeon@gmail.com>
2016-01-27 13:57:58 +0900

    x

--- application/controllers/File.php
+++ application/controllers/File.php
@@ -7,10 +7,6 @@ class File extends CI_Controller {
 	{
 		$uriPath = $this->uri->segment(3);
 		$idName = end(explode("=", $uriPath));
-/*		$fileName = $idName[1] . "_profile.jpg";
- 		$filename  = basename($_FILES['file']['name']);
-		$extension = pathinfo($filename, PATHINFO_EXTENSION);
-		$new       = $idName[1].'.'.$extension; */
 		
 		if (!empty($_FILES)) {
 			$ext = end(explode(".", $_FILES['userfile']['name']));
@@ -18,20 +14,5 @@ class File extends CI_Controller {
 			
 			move_uploaded_file($_FILES['userfile']['tmp_name'],SITE_ROOT."/files/profile/". $fileName);
 		}
-
-		// $config['upload_path'] = "/files/profile/";
-		//
-		// $this->load->library('upload', $config);
-		//
-		// if ( ! $this->upload->do_upload())
-		// {
-		// 	$error = array('error' => $this->upload->display_errors());
-		// 	echo "Sibal";
-		// }
-		// else
-		// {
-		// 	$data = array('upload_data' => $this->upload->data());
-		// 	echo "good";
-		// }
 	}
 }
