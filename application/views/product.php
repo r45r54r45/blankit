76c3440d8eed3f5efdb7d7f244184c152f978ac8
Woohyun Kim <r45r54r45@naver.com>
2016-01-27 15:30:06 +0900

    nn

--- application/views/product.php
+++ application/views/product.php
@@ -417,14 +417,15 @@ function makePrice(option){
   <input type="text" name="storeType" value="<?echo $GlobalStoreType;?>">
 </form>
 <script>
-function cl(){
+function facebook_for_artist(){
   FB.ui({
   method: 'share',
-  href: 'http://blankit.kr/store/product/5',
+  href: 'http://blankit.kr/funding/product/5',
 }, function(response){});
 }
 </script>
 <div class="row row_padding-xs" style="margin-top:20px;">
+  <a onclick="facebook_for_artist()">
   <div class="col-xs-12" style="background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAAqCAMAAABFoMFOAAAAWlBMV…8/UxBxQDQuFwlpqgBZBq6+P+unVY1GnDgwqbD2zGz5e1lBdwvGGPE6OgAAAABJRU5ErkJggg==);
     background-repeat: repeat-x;
     background-size: auto;
@@ -432,8 +433,8 @@ function cl(){
     background-color: #3a5795;
     border: 0;
     border-bottom: 1px solid #133783;
-    box-shadow: 0 2px 2px -2px rgba(0, 0, 0, .52); height:200px; border-radius:20px;">
-    <span style="font-size:30; font-weight:500; color:white; margin-top:30px;">페이스북에 공유하기 (클릭)</span>
+    box-shadow: 0 2px 2px -2px rgba(0, 0, 0, .52); height:200px; border-radius:20px; text-align:center; ">
+    <span style="font-size:30px; font-weight:500; color:white; ">페이스북에 공유하기 (클릭)</span>
   </div>
+</a>
 </div>
-<button onclick="cl()">dd</button>
