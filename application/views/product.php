4952a28ae3b55076dd675761972d21857892b191
Woohyun Kim <r45r54r45@naver.com>
2016-01-27 15:58:40 +0900

    ㅓ

--- application/views/product.php
+++ application/views/product.php
@@ -456,7 +456,7 @@ $PRICE =  number_format($storePrice, 0, '.', ','); // 가격 천 단위 콤마 �
         <div class="col-xs-12" style="
         background-color: #3a5795;
         box-shadow: 0 2px 2px -2px rgba(0, 0, 0, .52); height:100px; border-radius:20px; text-align:center; ">
-        <span style="font-size:30px; line-height: 100px; vertical-align:middle; font-weight:500; color:white; ">페이스북에 공유하기 (클릭)</span>
+        <span style="font-size:30px; line-height: 100px; vertical-align:middle; font-weight:500; color:white; ">페이스북에 공유하기</span>
       </div>
     </a>
 </div>
