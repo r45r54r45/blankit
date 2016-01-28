15470703a8fbeab57f8499ee3e73ca51747e0bbc
Woohyun Kim <r45r54r45@naver.com>
2016-01-28 19:19:46 +0900

    llk

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,8 +1,8 @@
 <!-- footer 시작 -->
 <div class="container-fluid">
 <div class="row">
-  <div class="col-md-12" style="margin-top:100px; background: black; height:250px;">
-
+  <div class="col-md-12" style="margin-top:100px; border-top:1px solid #D3D3D3; background: #D3D3D3;height:250px;text-align:center;">
+    <span style="display:table;margin:100px auto 0px; font-size:20px; color:white;">우리는 블랭킷 히힛 ㅎ</span>
   </div>
 </div>
 </div>
