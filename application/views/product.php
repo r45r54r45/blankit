0c872ae53a8376baacd9779d7b1b6aa9d61b1439
Woohyun Kim <r45r54r45@naver.com>
2016-01-28 21:02:16 +0900

    ddd

--- application/views/product.php
+++ application/views/product.php
@@ -485,7 +485,7 @@ $PRICE =  number_format($storePrice, 0, '.', ','); // 가격 천 단위 콤마 �
           padding-bottom: 20px;
           padding-top: 20px;">
           <div style="display:inline-block;">
-            <img src="<?php echo $artistProfile;?>" class="img img-circle img-responsive"
+            <img src="<?php echo $artistProfile;?>" class="pp img img-circle img-responsive"
             style="height:100px;">
           </div>
           <div id="hello" style="padding-left: 20px;display:inline-block;vertical-align: top;padding-top: 10px;">
