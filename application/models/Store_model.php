23e5be98a61c1e32d50b892e9c0955d4ec464d5e
ArsisCheon <arsischeon@gmail.com>
2016-01-28 13:40:54 +0900

    x

--- application/models/Store_model.php
+++ application/models/Store_model.php
@@ -8,7 +8,7 @@ class Store_model extends CI_Model{
 	
 	public function contents(){
 		return $this->db->query("
-		select store_image, store_day, store_goal, store_goal_now, artist_type, user_name, artist_profile, store_type, store_status, s.store_id, store_explain
+		select store_name, store_image, store_day, store_goal, store_goal_now, artist_type, user_name, artist_profile, store_type, store_status, s.store_id, store_explain
 		from STORE s, USER u
 		where s.user_id = u.user_id;
 		");
