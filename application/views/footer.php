15bf196c7fb6ceac8587554ab9ba4b82a208db85
ArsisCheon <arsischeon@gmail.com>
2016-01-29 15:18:38 +0900

    x

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,7 +2,7 @@
 <div class="container-fluid">
 <div class="row">
   <div class="col-md-12" style="margin-top:100px; border-top:1px solid #D3D3D3; background: #D3D3D3;height:250px;text-align:center;">
-    <span style="display:table;margin:100px auto 0px; font-size:20px; color:white;">우리는 블랭킷 히힛 ㅎ</span>
+    <span style="display:table;margin:100px auto 0px; font-size:20px; color:white;">© 2016 blankit ALL RIGHTS RESERVED</span>
   </div>
 </div>
 </div>
