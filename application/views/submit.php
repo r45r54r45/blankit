17a119239f89aa753cc187964c36c3bbd396bb64
Woohyun Kim <r45r54r45@naver.com>
2016-02-05 15:31:34 +0900

    ddd

--- application/views/submit.php
+++ application/views/submit.php
@@ -166,9 +166,11 @@ else {
   </div>
   <div class="row">
     <div class="col-xs-12" style="margin-top:30px; border-top: 1px solid #D3D3D3;">
+      <a href="https://drive.google.com/file/d/0B-DTjWKJzjjsR0NRU3JMS29teG8/view?usp=sharing">
       <span style="margin-top:30px; text-align:center; display:block; font-size: 25px; color:#655453; font-weight:bold;">
         아티스트 가이드북 다운로드
       </span>
+    </a>
     </div>
   </div>
   <div class="row" style="margin-top:30px; margin-bottom:30px; text-align:center;">
