66e47871798f919d4acb693a9dc0df80c52fb90c
Woohyun Kim <r45r54r45@naver.com>
2016-02-05 17:07:09 +0900

    ddd

--- application/models/Store_model.php
+++ application/models/Store_model.php
@@ -5,23 +5,23 @@ class Store_model extends CI_Model{
 	public function __construct(){
 		$this->load->database();
 	}
-	
+
 	public function contents(){
 		return $this->db->query("
 		select store_name, store_image, store_day, store_goal, store_goal_now, artist_type, user_name, artist_profile, store_type, store_status, s.store_id, store_explain, artist_intro
 		from STORE s, USER u
-		where s.user_id = u.user_id;
+		where s.user_id = u.user_id order by store_id desc;
 		");
 	}
-	
+
 	public function details(){
 		return $this->db->query("
 		select store_image, store_day, store_goal, store_goal_now, user_name, store_type, store_status, s.store_id, store_name, store_price
 		from STORE s, USER u
-		where s.user_id = u.user_id;
+		where s.user_id = u.user_id order by store_id desc;
 		");
 	}
-	
+
 	public function toCart($type, $color, $size, $amount, $storeId, $storeType){
 		if($storeType == 1){
 			if($type == 1){
@@ -93,13 +93,13 @@ class Store_model extends CI_Model{
 			else if($type ==2){
 				$init = 20000;
 				$type = "대형";
-			}	
+			}
 		}
 		$price = $amount * $init; // 개당 가격 * 수량
 		$user_id_SESSION = $this->session->userdata('user_id');
-		
+
 		$date = date('Y-m-d H:i:s'); // 카드 담은 시간
-		
+
 		$this->db->query("
 		INSERT INTO CART (user_id, store_id, cart_type, cart_color, cart_size, cart_num, cart_price, cart_price_total, cart_time)
 		VALUES ('$user_id_SESSION','$storeId','$type','$color','$size','$amount','$init','$price', '$date');
