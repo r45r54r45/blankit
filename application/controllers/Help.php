9f73b7dedd3ef2608d15d5358860b13696aa31dd
ArsisCheon <arsischeon@gmail.com>
2016-02-10 20:36:55 +0900

    ㅌ

--- application/controllers/Help.php
+++ application/controllers/Help.php
@@ -48,7 +48,7 @@ class Help extends CI_Controller {
 			$this->email->cc('ryusooyon@gmail.com, yunjiljjh@gmail.com, r54r45r54@gmail.com');
 
 			$emailSubject = 'CS 문의 접수 - ' . $title;
-			$emailMessage = "문의가 접수 되었어!\n\n\n\n접수 이메일: " . $email . "\n\n문의 제목: " . $title . "\n\n문의 내용: " . $contents;
+			$emailMessage = "문의가 접수 되었어!\n\n----------\n\n접수 이메일: " . $email . "\n\n문의 제목: " . $title . "\n\n문의 내용: " . $contents;
 			
 			$this->email->subject($emailSubject);
 			$this->email->message($emailMessage);
