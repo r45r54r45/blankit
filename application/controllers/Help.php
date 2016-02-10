c1aaeb1339a0cae46469151eeee537d7c8fce7ae
ArsisCheon <arsischeon@gmail.com>
2016-02-10 20:05:37 +0900

    x

--- application/controllers/Help.php
+++ application/controllers/Help.php
@@ -22,8 +22,8 @@ class Help extends CI_Controller {
 		$this->load->view('footer');
 	}
 	public function func_cs_submit(){
-		//로그인 상태 및 post 데이터가 있어야함
-		if($this->session->userdata('user_id') && $this->input->post('title') && $this->input->post('contents')){
+		// post 데이터가 있어야함
+		if($this->input->post('title') && $this->input->post('contents')){
 			$title = $this->input->post('title');
 			$contents = $this->input->post('contents');
 			
