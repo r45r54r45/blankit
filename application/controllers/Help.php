0bd306ea48ce86f6574c084f79b8f5ca02d635cf
ArsisCheon <arsischeon@gmail.com>
2016-02-10 20:30:56 +0900

    ㅌ

--- application/controllers/Help.php
+++ application/controllers/Help.php
@@ -45,10 +45,10 @@ class Help extends CI_Controller {
 			
 			$this->email->from('do-not-reply@blankit.kr', 'Customer Service');
 			$this->email->to('arsischeon@gmail.com');
-			//$this->email->cc('yunjiljjh@gmail.com', 'ryusooyon@gmail.com', 'r54r45r54@gmail.com');
+			$this->email->cc('yunjiljjh@gmail.com', 'ryusooyon@gmail.com', 'r54r45r54@gmail.com');
 
 			$emailSubject = 'CS 문의 접수 - ' . $title;
-			$emailMessage = "문의가 접수 되었어! 확인해보자. 다음과 같은 내용이야: \n\n" . $email . "\n\n" . $contents;
+			$emailMessage = "문의가 접수 되었어! 확인해보자. 다음과 같은 내용이야: \n\n접수 이메일: " . $email . "\n\n문의 내용: " . $contents;
 			
 			$this->email->subject($emailSubject);
 			$this->email->message($emailMessage);
