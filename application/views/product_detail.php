890f4d07482c4df043c2ff06bb6e387dceb2156c
Woohyun Kim <r45r54r45@naver.com>
2016-02-18 20:03:27 +0900

    ddd

--- application/views/product_detail.php
+++ application/views/product_detail.php
@@ -193,9 +193,10 @@
     <span style="margin:0 auto; display:table;">작 품 해 설</span>
   </div>
   <div class="col-xs-12" style="margin-top:20px;" >
-    <span style="margin:0 auto; width:30px; display:table; border-bottom:1px solid black;">    </span>
+    <span style="margin:0 auto; width:30px; display:table; border-bottom:2px solid #D3D3D3;">    </span>
   </div>
-  <div class="col-xs-12" style="margin-top:20px;" >
+  <div class="col-xs-12" style="margin-top:20px;border-bottom: 1px solid #D3D3D3;
+    padding-bottom: 30px;" >
   <div class="t2">
     <?echo $storeExplain;?>
   </div>
