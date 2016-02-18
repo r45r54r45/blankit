b8e1512c354ae16e4129c5ea386b91d1a39b3e22
Woohyun Kim <r45r54r45@naver.com>
2016-02-18 19:18:06 +0900

    ㅇㅇ

--- application/views/join_complete.php
+++ application/views/join_complete.php
@@ -58,8 +58,7 @@
       </div>
       <div class="col-xs-12" style="margin-top:30px; ">
         <span style="margin-top:10px; text-align:center; display:block; font-size: 18px; color:#655453; font-weight:400;">
-          아티스트는 페이스북에 블랭킷과의 콜라보레이션 사실을 공유해주셔야 할 의무가 있습니다.<br>
-          펀딩 기간 종료시까지 기재해주셔야 하며, 미실시 및 펀딩 기간 내에 삭제시 페널티가 있습니다.
+          환영합니다.
         </span>
       </div>
   </div>
