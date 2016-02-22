eb5093f4ffcc5d6931d96188c60c1849b34d2405
ArsisCheon <arsischeon@gmail.com>
2016-02-22 20:06:28 +0900

    x

--- application/models/Submit_model.php
+++ application/models/Submit_model.php
@@ -20,12 +20,12 @@ class Submit_model extends CI_Model{
 			$curDateNum = strtotime($curDate);
 			$seed_date = strtoupper(dechex($curDateNum + $randomID));
 						
-			$date =  date('Y-m-d 0:0:0', strtotime("+7 days"));
+			$date =  date('Y-m-d 0:0:0', strtotime("+365 days"));
 			
 			$this->db->query("
 				insert into `STORE` (user_id, store_name, store_random_id, store_day, store_type, store_goal, store_status, store_explain, store_pref)
-				values ('$user_id_SESSION', '$artwork', '$seed_date', '$date', '$storeType', '$storeGoal', '1', '$explain', '$pref')
-			");
+				values ('$user_id_SESSION', '$artwork', '$seed_date', '$date', '$storeType', '$storeGoal', '0', '$explain', '$pref')
+			");  // 새 작품 업로드를 0 상태로 임시 변경 16.02.22
 		}
 	}
 	
