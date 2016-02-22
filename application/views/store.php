142beeeff2289353fba4c0e4379a817f7e379de1
ArsisCheon <arsischeon@gmail.com>
2016-02-22 20:35:53 +0900

    x

--- application/views/store.php
+++ application/views/store.php
@@ -86,9 +86,9 @@
     <div class="col-md-12" style="    margin-top: 30px; position:relative; height:25px; border-bottom: 1.5px solid #D3D3D3;">
       <div class="menu">
         <a href="/funding"><div class="menu_item" style="border-bottom:2px solid #96AFB5; color:#96AFB5;">전체</div></a>
-        <a href="/funding/cloth"><div class="menu_item" >의류</div></a>
+<!--         <a href="/funding/cloth"><div class="menu_item" >의류</div></a>
         <a href="/funding/ecobag"><div class="menu_item">에코백</div></a>
-      <a href="/funding/pouch">  <div class="menu_item">파우치</div></a>
+      	<a href="/funding/pouch"><div class="menu_item">파우치</div></a> -->
         <a href="/funding/done"><div class="menu_item">종료</div></a>
       </div>
     </div>
